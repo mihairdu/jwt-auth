bf5a43650b5e66edfb98f4f78e795631e35b2d05
Mihai-Daniel Radu <mihaidaniel.radu00@gmail.com>
2020-12-26 21:51:35 +0200

    Added post api - no auth for now

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
+
+dotenv.config();
+
+//import routes
+const authRoute = require('./routes/auth');
+
+//connect to DB
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useUnifiedTopology: true, useNewUrlParser: true },
+    () => {console.log('Connected to DB')
+});
+
+//Middleware
+app.use(express.json());
+
+//route Middlewares
+app.use('/api/user', authRoute);
 
 app.listen(3030, () => {
     console.log("Server up and running");
